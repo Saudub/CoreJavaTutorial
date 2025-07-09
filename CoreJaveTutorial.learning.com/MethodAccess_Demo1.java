9b1dc58104298329e9dadddd5c1bf5030bf0a5e3
Saurabh Dubey <motivated.saurabh@gmail.com>
2025-07-09 14:10:43 +0530

    Commit from my Dell Laptop

---
+++ CoreJaveTutorial.learning.com/MethodAccess_Demo1.java
@@ -0,0 +1,16 @@
+
+public class MethodAccess_Demo1 {
+	
+	public void Multiplication() {
+		
+		int d =4*10;
+		System.out.println(d);
+	}
+
+	public static int division() {
+		
+		int e = 10/2;
+		return e;
+		
+	}
+}
